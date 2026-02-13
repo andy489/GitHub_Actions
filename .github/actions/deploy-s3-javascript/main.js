bd708364c6da03cc0bba19325a13e3f4f95d3f7e
Stoevand-flutterstudios <andrey_stoev@flutterstudios.com>
2026-02-13 23:27:19 +0200

    added custom workflow

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,18 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
+// const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 async function run() {
+    // 1) Gove some input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2) Upload files
+    const s3Uri = `s3://${bucket}`
+    AWS_ACCESS_KEY_ID=AKIAWRU4UZYWB266LQXD
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
     core.notice('Hello from my custom JavaScript Action!');
 }
 
